646e8001354a4a75c49e5adbcdf877f4b6b39687
Julian-Alcaraz <62039908+Julian-Alcaraz@users.noreply.github.com>
2024-05-30 19:46:40 -0300

    Arreglo para arrancar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -170,10 +170,4 @@
         </div>
     </body>
 </html>
-<!-- <li><a href="{{ route('home') }}">Home</a></li>
-        <li><a href="/auth">login</a></li>
-        <li><a href="/logout">logout</a></li>
-        <li><a href="{{ route('category') }}">list category</a></li>
-        <li><a href="{{ route('categoryShow', ['id' => 5]) }}">show category</a></li>
-        <li><a href="{{ route('categoryCreate') }}">create category</a></li>
-        <li><a href="{{ route('categoryEdit', ['id' => 5]) }}">edit category</a></li> -->
+
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,38 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'getHome'])->name("home");
-
-Route::get('auth/', function () {
-  return view('auth/login');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('logout/', function () {
-  return view('logout');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/', [HomeController::class, 'getHome'])->name("home");
+
+// Route::get('auth/', function () {
+//   return view('auth/login');
+// });
+
+// Route::get('logout/', function () {
+//   return view('logout');
+// });
 
 Route::get('category/', [CategoryController::class, 'getIndex'])->name("category");
 Route::get('category/show/{id}', [CategoryController::class, 'getShow'])->name("categoryShow");
